bc68ff2273f56f24d2eafb358b5e0f62e1a88e44
Duk3lo <jhosue10avecillas@gmail.com>
2026-04-18 14:56:17 -0500

    update

--- src/main/java/org/astral/spectyle/hytale/events/event/Disconnect.java
+++ src/main/java/org/astral/spectyle/hytale/events/event/Disconnect.java
@@ -2,48 +2,43 @@ package org.astral.spectyle.hytale.events.event;
 
 import com.hypixel.hytale.event.EventRegistry;
 import com.hypixel.hytale.logger.HytaleLogger;
-import com.hypixel.hytale.server.core.HytaleServer;
 import com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent;
 import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
 import org.astral.spectyle.audio.api.AudioAPI;
 import org.astral.spectyle.audio.engine.AudioEngine;
+import org.astral.spectyle.hytale.commands.command.Play;
 import org.astral.spectyle.hytale.events.visuals.VisualizerManager;
 import org.astral.spectyle.hytale.events.visuals.world.AudioBarsBlocks;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 public final class Disconnect {
     public static void register(@NotNull EventRegistry registry, HytaleLogger logger, AudioEngine engine){
         registry.registerGlobal(PlayerDisconnectEvent.class, event -> {
-            World tempWorld = null;
-            try {
-                UUID uuid = event.getPlayerRef().getWorldUuid();
-                if (uuid == null)return;
-                tempWorld = Universe.get().getWorld(uuid);
-            } catch (Exception ignored) {}
-
-            final World world = tempWorld;
-
-            HytaleServer.SCHEDULED_EXECUTOR.schedule(()->{
-                int allPlayers = Universe.get().getPlayerCount();
-                if (allPlayers < 1) {
-                    if (AudioAPI.isPlaying()){
-                        logger.atInfo().log("There are no players, the music is stopping...");
-                        engine.stopSong();
-                    }
-                    if (world != null) {
-                        for (VisualizerManager.VisualizerData data : VisualizerManager.getAllGlobalData()) {
-                            if (data.getType().equals("blocks")) {
-                                AudioBarsBlocks.stopAndReset(world, data);
+            UUID disconnectedPlayerUuid = event.getPlayerRef().getUuid();
+            Play.getPlayersListen().remove(disconnectedPlayerUuid);
+            if (Play.getPlayersListen().isEmpty()) {
+                if (AudioAPI.isPlaying()){
+                    logger.atInfo().log("The last listener disconnected, the music is stopping...");
+                    engine.stopSong();
+                }
+                try {
+                    UUID worldUuid = event.getPlayerRef().getWorldUuid();
+                    if (worldUuid != null) {
+                        World world = Universe.get().getWorld(worldUuid);
+                        if (world != null) {
+                            for (VisualizerManager.VisualizerData data : VisualizerManager.getAllGlobalData()) {
+                                if (data.getType().equals("blocks")) {
+                                    AudioBarsBlocks.stopAndReset(world, data);
+                                }
                             }
                         }
                     }
-                    VisualizerManager.stopAllGlobally();
-                }
-            }, 1L, TimeUnit.SECONDS);
+                } catch (Exception ignored) {}
+                VisualizerManager.stopAllGlobally();
+            }
         });
     }
 }
