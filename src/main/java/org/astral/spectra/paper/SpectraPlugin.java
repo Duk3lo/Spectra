d8ee77a7404814d77040ea77311db55cf9c8b813
Duk3lo <jhosue10avecillas@gmail.com>
2026-06-03 14:19:05 -0500

    updates

---
+++ src/main/java/org/astral/spectra/paper/SpectraPlugin.java
@@ -0,0 +1,17 @@
+package org.astral.spectra.paper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class SpectraPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
